3897439f352673f66b77b004de412c9fe3a5e433
Vlad Vinskevitch <vvinskevitch@gmail.com>
2021-05-16 13:21:08 +0300

    add new fragment Manual_calculator

---
+++ app/src/main/java/com/sharkit/nextmonday/ui/settings/Manual_calculate_calorie.java
@@ -0,0 +1,24 @@
+package com.sharkit.nextmonday.ui.settings;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sharkit.nextmonday.R;
+
+public class Manual_calculate_calorie extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+       View root = inflater.inflate(R.layout.calculator_manual_calculate_calorie, container, false);
+
+
+        return root;
+
+    }
