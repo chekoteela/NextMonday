f914c13ec3ac5a90a99866968ffa02e55bcd215e
chaosmaster <vvinskevitch@gmail.com>
2021-07-02 16:07:38 +0300

    ignore

--- app/src/main/java/com/sharkit/nextmonday/Configuration/AlarmDiary.java
+++ app/src/main/java/com/sharkit/nextmonday/Configuration/AlarmDiary.java
@@ -3,30 +3,42 @@ package com.sharkit.nextmonday.Configuration;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
 
+import com.sharkit.nextmonday.MainMenu;
 import com.sharkit.nextmonday.R;
 
-public class AlarmDiary extends NotificationCompat {
-    Context context;
-    final String CHANNEL_ID = "1";
-    public void Alarm(){
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context,CHANNEL_ID);
-        builder.setSmallIcon(R.drawable.logo)
-                .setContentTitle("Next Monday")
-                .setContentText("У вас не завершеные задачи")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-    }
-    private void createNotificationChannel(){
+public class AlarmDiary extends BroadcastReceiver {
+
+    private final String CHANNEL_ID = "NextMonday";
+    @Override
+    public void onReceive(Context context, Intent intent) {
+//        MediaPlayer mediaPlayer = MediaPlayer.create(context, Settings.System.DEFAULT_RINGTONE_URI);
+//        mediaPlayer.start();
+        String massage = "Не забудьте выполнить";
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.logo_white)
+                .setContentTitle("У вас есть запланированное событие")
+                .setContentText(massage)
+                .setAutoCancel(true);
+        Intent intent2 = new Intent(context, MainMenu.class);
+        intent2.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent2.putExtra("message",massage);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        builder.setContentIntent(pendingIntent);
         NotificationManager notificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "NextMonday",
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "My channel",
                     NotificationManager.IMPORTANCE_HIGH);
             channel.setDescription("My channel description");
             channel.enableLights(true);
@@ -34,5 +46,7 @@ public class AlarmDiary extends NotificationCompat {
             channel.enableVibration(false);
             notificationManager.createNotificationChannel(channel);
         }
+        notificationManager.notify(0,builder.build());
+
     }
 }
